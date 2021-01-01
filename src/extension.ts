9511e0cb4b965f24eeb43ae149c8a485a95e068f
William Lorey <wilorey@microsoft.com>
2020-12-31 18:15:48 -0600

    Fix unstoppable party mode
    Remove unneeded workspace function

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,7 @@
 import { ExtensionContext, commands, window, workspace, ConfigurationTarget } from 'vscode';
 import { hexDigits, configOptions, colorLength } from './constants';
 
-let intervalId: NodeJS.Timeout;
+let intervalId: NodeJS.Timeout | undefined;
 
 // Returns a random color in hex format
 function getRandHexColor(): string {
@@ -12,48 +12,32 @@ function getRandHexColor(): string {
     return color;
 }
 
-// Returns the user's workspace configuration scoped to 'workbench'
-// Informs the user if unsuccessful
-function getWorkspaceConfiguration(): any {
-    // Attempt to get a reference to the user's active editor
-    let activeTextEditor = window.activeTextEditor;
-
-    if (activeTextEditor === undefined) {
-        // Not sure how to access configuration without an active text editor
-        window.showErrorMessage('Open a file first.');
-        return null;
-    } else {
-        // Configuration can be found
-        return workspace.getConfiguration('workbench', activeTextEditor.document.uri);
-    }
-}
-
 export function activate(context: ExtensionContext) {
     commands.registerCommand('extension.getRandomTheme', () => {
-        let workspaceConfiguration = getWorkspaceConfiguration();
+        // Map configuration string to random color string
+        let colorConfigMap: { [option: string]: string } = {};
+        configOptions.forEach(option => (colorConfigMap[option] = getRandHexColor()));
 
-        if (workspaceConfiguration !== null) {
-            // Map configuration string to random color string
-            let colorConfigMap: { [option: string]: string } = {};
-            configOptions.forEach(option => (colorConfigMap[option] = getRandHexColor()));
-
-            // Update workspace configuration with new colors
-            workspaceConfiguration.update('colorCustomizations', colorConfigMap, ConfigurationTarget.Workspace);
-        }
+        // Update workspace configuration with new colors
+        workspace.getConfiguration('workbench').update('colorCustomizations', colorConfigMap, ConfigurationTarget.Workspace);
     });
 
     commands.registerCommand('extension.removeRandomTheme', () => {
-        clearInterval(intervalId);
-
-        let workspaceConfiguration = getWorkspaceConfiguration();
-        if (workspaceConfiguration !== null) {
-            // Clear workspace color configuration
-            workspaceConfiguration.update('colorCustomizations', {}, ConfigurationTarget.Workspace);
+        if (intervalId !== undefined) {
+            clearInterval(intervalId);
+            intervalId = undefined;
         }
+
+        // Clear workspace color configuration
+        workspace.getConfiguration('workbench').update('colorCustomizations', {}, ConfigurationTarget.Workspace);
     });
 
     commands.registerCommand('extension.partyModeOn', () => {
-        intervalId = setInterval(() => commands.executeCommand('extension.getRandomTheme'), 500);
+        if (intervalId === undefined) {
+            // Only set the interval if it's not set already
+            // Otherwise unstoppable party mode is a reality 😬
+            intervalId = setInterval(() => commands.executeCommand('extension.getRandomTheme'), 500);
+        }
     });
 
     commands.registerCommand('extension.partyModeOff', () => {
